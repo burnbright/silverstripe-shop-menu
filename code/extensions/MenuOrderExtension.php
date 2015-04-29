7f37aa42a1c0b5cff42462db426c20a0447d9d41
Jeremy Shipman <jeremy@burnbright.net>
2015-04-29 15:35:57 +1200

    Changed method name so that it would work properly in templates

--- code/extensions/MenuOrderExtension.php
+++ code/extensions/MenuOrderExtension.php
@@ -2,13 +2,16 @@
 
 class MenuOrderExtension extends DataExtension{
 		
-	//allow items to be grouped	
-	function getMenuGroupableItems(){
+	/**
+	 * Allow items to be grouped
+	 * @param boolean $editable
+	 */
+	public function MenuGroupableItems(){
 		$items = $this->owner->Items()
 				->leftJoin("Product_OrderItem", "Product_OrderItem.ID = OrderAttribute.ID")
 				->leftJoin("MenuProductSelection", "Product_OrderItem.MenuProductSelectionID = MenuProductSelection.ID")
 				->leftJoin("Menu", "Menu.ID = MenuProductSelection.MenuID");
-		
+
 		return new Menu_GroupedList($items);
 	}
 
